083a32320d25673909cfded40a6aa822abd5233a
Shiwani Kumari <64477234+Shiwani19890@users.noreply.github.com>
2020-08-20 13:35:02 +0530

    Add files via upload

---
+++ life,the universe and everything.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    while(a<100 && a!=42)
+    {
+        cout<<a<<endl;
+        return main();
+    }
+
+    
+	return 0;
+}
